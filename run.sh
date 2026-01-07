4a95de810ec1666d13360c5c23c42f670102568d
wpsend <hostserverbd24@gmail.com>
2026-01-07 22:56:35 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,39 +1,23 @@
 #!/bin/bash
 
-base="RcLicense"
+RcLicenseCP \
+& RcLicenseLSWS \
+& RcLicenseCLN \
+& RcLicenseImunify360 \
+& RcLicenseWP \
+& RcLicenseSoftaculous \
+& RcLicenseSitepad \
+& RcLicensePlesk \
+& RcLicenseVirtualizor \
+& RcLicenseJetBackup \
+& RcLicenseWHMReseller \
+& RcLicenseWHMSonic \
+& RcLicenseDA \
+& RcLicenseDAReseller \
+& RcLicenseOSM \
+& RcLicenseCXS \
+& RcLicenseCPGuard \
+& RcLicenseCP-fleetssl
 
-services=(
-CP
-LSWS
-CLN
-Imunify360
-WP
-Softaculous
-Sitepad
-Plesk
-Virtualizor
-JetBackup
-WHMReseller
-WHMSonic
-DA
-DAReseller
-OSM
-CXS
-CPGuard
-CP-fleetssl
-)
-
-for service in "${services[@]}"; do
-    name="${base}${service}"
-
-    echo "Starting $name"
-    
-    # এখানে common কাজটা হবে
-    # উদাহরণ হিসেবে শুধু delay দিলাম
-    sleep 2
-
-    echo "$name finished"
-    echo "---------------------"
-done
-
-echo "All license process completed"
+wait
+echo "All RcLicense processes started"
